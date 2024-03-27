74a34c3aa7a5868e14d06ef7dd1fcee2e80da436
abelbouchikhi <abderrahman.elbouchikhi1337@gmail.com>
2024-03-27 23:36:17 +0000

    implemeted Email Verification and Password Reset (to be Reviewed)

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -6,11 +6,14 @@ const {
   getUserProfile,
   updateUserProfile,
   resetPassword,
+  verifyEmail,
 } = require("../controllers/users.controllers");
+const { verify } = require("crypto");
 
 usersRouter.post("/register", registerUser); // User registration
 usersRouter.post("/login", loginUser); // User login
-// usersRouter.post("/resetPassword", resetPassword); // Reset User Password
+usersRouter.post("/resetPassword", resetPassword); // Reset User Password
+usersRouter.get("/verify/:token", verifyEmail);
 // usersRouter.get("/profile", getUserProfile); // Get user profile
 // usersRouter.put("/profile", updateUserProfile); // Update user profile
 
