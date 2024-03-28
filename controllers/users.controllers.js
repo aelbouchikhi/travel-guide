8867d8949ca01e82e907372d3f68729a98da3375
larbiexpo <larbiexpo@gmail.com>
2024-03-28 13:57:28 +0100

    create middleware isAuth and (update,delete,getall) users

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -4,7 +4,7 @@ const {generateToken} = require('../helpers/jwt')
 const {hashedPassword, matshPassword} = require('../helpers/hashPassword.helpers');
 const { findUseremail} = require('../helpers/findUserEmail.helpers');
 const userSchema = require('../models/schema/user.schema')
-
+const jwt = require('jsonwebtoken')
 
 exports.registerUser = async(req,res)=>{
     try{
@@ -45,5 +45,48 @@ exports.loginUser = async(req,res)=>{
     }catch(err){
         console.log(err)
         return res.status(401).send("Unauthorized");
+    }
+}
+exports.getUserProfile = async (req,res)=>{
+    const {id} = req.user;
+    try{
+        const userProfile = await userSchema.findById(id)
+        if(userProfile){
+            return res.json(userProfile);
+        }else{
+            return res.status(404).json({message:'Profil introuvable'})
+        };
+    }catch(err){
+        console.log(err)
+        res.status(500).json({message: 'Erreur de serveur'})
+    }
+}
+exports.updateUserProfile = async(req,res)=>{
+    const id = req.user.id
+    try{
+        const updateProile = await userSchema.updateOne({_id: id}, {username, email, password,age,sex,country,phoneNumber}, {new: true})
+        if(updateProile){
+            return res.status(200).json(updateProfile);
+        }else{
+            return res.status(404).json({ message: 'Profil introuvable' });
+        }
+        
+    }catch(err){
+        console.log(err);
+        return res.status(500).json({ message: 'Erreur de serveur' });
+    }
+}
+exports.deleteUserProfile = async()=>{
+    const id = req.user.id;
+    try{
+        const deleteProfile = await userSchema.deleteOne({_id: id})
+        if(deleteProfile.deletedCount > 0){
+            return res.status(200).json({ message: 'Profil supprimé avec succès' });
+        }else{
+            return res.status(404).json({ message: 'Profil introuvable' });
+        }
+    }catch(err){
+        console.log(err)
+        return res.send(500).json({ message: 'Erreur de serveur' });
     }
 }
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -5,13 +5,16 @@ const {
   loginUser,
   getUserProfile,
   updateUserProfile,
+  deleteUserProfile,
   resetPassword,
 } = require("../controllers/users.controllers");
+const { isAuth } = require("../middleware/isAuth.middleware");
 
 usersRouter.post("/register", registerUser); // User registration
 usersRouter.post("/login", loginUser); // User login
 // usersRouter.post("/resetPassword", resetPassword); // Reset User Password
-// usersRouter.get("/profile", getUserProfile); // Get user profile
-// usersRouter.put("/profile", updateUserProfile); // Update user profile
+usersRouter.get("/profile", isAuth, getUserProfile); // Get user profile
+usersRouter.put("/profile",isAuth, updateUserProfile); // Update user profile
+usersRouter.delete("/profile",isAuth, deleteUserProfile); // Delete user profile
 
 module.exports = usersRouter;
