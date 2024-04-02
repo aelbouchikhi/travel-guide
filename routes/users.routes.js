a0ac052ce8bdec2af0c80c4c450f5eb3adeeb9d0
yataji <yassinoubil3@yahoo.com>
2024-04-02 11:02:51 +0000

    multer

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -9,10 +9,9 @@ const {
   resetPassword,
   verifyEmail,
 } = require("../controllers/users.controllers");
-const { upload } = require("../middleware/multer.helpers");
 const { xssMiddleware } = require("../middleware/xss.middleware");
 const { validatorMiddleware } = require("../middleware/expresssValidator.middleware");
-const { upload } = require("../helpers/multer.helpers");
+const { upload } = require("../middleware/multer.middleware");
 const { isAuth } = require("../middleware/isAuth.middleware");
 
 usersRouter.post("/register", upload.single("images"), xssMiddleware, validatorMiddleware, registerUser); // User registration
@@ -23,7 +22,7 @@ usersRouter.get("/verify/:token", verifyEmail);
 // usersRouter.put("/profile", updateUserProfile); // Update user profile
 // usersRouter.post("/resetPassword", resetPassword); // Reset User Password
 usersRouter.get("/profile", isAuth, getUserProfile); // Get user profile
-usersRouter.put("/profile",isAuth, updateUserProfile); // Update user profile
-usersRouter.delete("/profile",isAuth, deleteUserProfile); // Delete user profile
+usersRouter.put("/profile", isAuth, updateUserProfile); // Update user profile
+usersRouter.delete("/profile", isAuth, deleteUserProfile); // Delete user profile
 
 module.exports = usersRouter;
